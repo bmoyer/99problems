f31d356d54d133f67d1b22965e463c32781dd469
Ben Moyer <ben.s.moyer@gmail.com>
2014-09-27 15:15:31 -0400

    9 working besides last group, saving

--- 9.hs
+++ 9.hs
@@ -2,4 +2,5 @@ pack str stack list
     | str == [] = list
     | null stack = pack (tail str) (stack ++ ([head str])) list
     | (last stack) == (head str) = pack (tail str) (stack ++ ([head str])) list
-    | (last stack) != (head str) = pack (tail str) (stack ++ ([head str])) (list : stack)
+    | (last stack) /= (head str) = pack (tail str) [head str] (list ++ [stack])
+    | otherwise = [[0]]
