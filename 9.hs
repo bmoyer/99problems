4315f00b0a65ea58882a84b898b05ddb0804ecde
Ben Moyer <ben.s.moyer@gmail.com>
2014-09-27 16:24:18 -0400

    9) working for non-string lists

--- 9.hs
+++ 9.hs
@@ -1,5 +1,6 @@
 pack str stack list
-    | str == [] = list
+    | null str && null stack = list
+    | null (str) = pack str [] (list ++ [stack])
     | null stack = pack (tail str) (stack ++ ([head str])) list
     | (last stack) == (head str) = pack (tail str) (stack ++ ([head str])) list
     | (last stack) /= (head str) = pack (tail str) [head str] (list ++ [stack])
